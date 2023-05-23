3b95cc23d40bb56def65af87de42926b1fc9276a
Gabriel <gabeguimaraes13@gmail.com>
2023-05-23 17:41:36 -0300

    refactor: tweak default layout

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Theme } from "@mui/material";
 import { makeStyles } from "@mui/styles";
 import Navbar from "../navbar/Navbar";
+import NavbarWarning from "../navbar/NavbarWarning";
+import NavbarCategories from "../navbar/NavbarCategories";
+import Footer from "../footer/Footer";
 
 interface DefaultLayoutProps {
   element: React.ReactNode;
@@ -15,12 +18,9 @@ const useStyles = makeStyles<Theme>((theme) => ({
     width: "100%",
   },
   nav: {
-    top: 0,
-    zIndex: 22,
     boxShadow:
       "rgba(0, 0, 0, 0.2) 0px 2px 4px -1px, rgba(0, 0, 0, 0.14) 0px 4px 5px 0px, rgba(0, 0, 0, 0.12) 0px 1px 10px 0px",
     minHeight: 64,
-    position: "sticky",
     padding: "15px 30px",
     background: theme.palette.primary.main,
   },
@@ -28,7 +28,7 @@ const useStyles = makeStyles<Theme>((theme) => ({
     width: "100%",
     overflow: "auto",
     minHeight: "calc(100vh - 104px)",
-    padding: "20px 30px",
+    padding: "0px 30px 20px 30px",
     position: "relative",
     display: "flex",
     flexDirection: "column",
@@ -46,11 +46,17 @@ const DefaultLayout: React.FC<DefaultLayoutProps> = ({ element }) => {
 
   return (
     <div className={classes.container}>
-      <nav className={classes.nav}>
-        <Navbar />
-      </nav>
+      <header>
+        <NavbarWarning />
+        <nav className={classes.nav}>
+          <Navbar />
+        </nav>
+        <NavbarCategories />
+      </header>
 
       <div className={classes.content}>{element}</div>
+
+      <Footer />
     </div>
   );
 };
