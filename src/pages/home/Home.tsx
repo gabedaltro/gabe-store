4514baba66cc3f9e6e345485cd5e45e0bb5e18ea
Gabriel <gabeguimaraes13@gmail.com>
2023-05-23 17:43:09 -0300

    refactor: improve home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,9 @@
 import { makeStyles } from "@mui/styles";
 import React from "react";
+import ImageSlider from "../../components/image-slider/ImageSlider";
+import { useApp } from "../../hooks/app";
+import { AnimatedBackground } from "../../styles/animatedBackground";
+import FeaturedProducts from "../../components/featured-products/FeaturedProducts";
 
 const useStyles = makeStyles({
   container: {
@@ -7,20 +11,35 @@ const useStyles = makeStyles({
     flexDirection: "column",
   },
   slider: {
+    maxWidth: 960,
+    width: "100%",
+    margin: "0 auto",
     height: 400,
   },
   products: {
-    display: "flex",
+    padding: "0 40px",
   },
 });
 
 const Home: React.FC = () => {
   const classes = useStyles();
+  const { loading, products } = useApp();
 
   return (
     <div className={classes.container}>
-      <div className={classes.slider}>aqui vai ter uma slider</div>
-      <div className={classes.products}>aqui aparecerão 4 produtos</div>
+      <div className={classes.slider}>
+        {loading ? (
+          <AnimatedBackground style={{ height: 400 }} />
+        ) : (
+          <ImageSlider />
+        )}
+      </div>
+      <div className={classes.products}>
+        <FeaturedProducts
+          products={products.slice(0, 12)}
+          title="em destaque"
+        />
+      </div>
       <div className={classes.slider}>aqui vai ter uma slider</div>
       <div className={classes.slider}>aqui vai ter uma slider</div>
     </div>
