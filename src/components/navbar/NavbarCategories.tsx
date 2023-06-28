62cd713a46141f4602f1d7decf2846218c3e5d80
Gabriel <gabeguimaraes13@gmail.com>
2023-06-28 15:30:23 -0300

    refactor: navbarcategories

--- src/components/navbar/NavbarCategories.tsx
+++ src/components/navbar/NavbarCategories.tsx
@@ -1,73 +1,69 @@
 import React, { useState, useEffect } from "react";
 import { useApp } from "../../hooks/app";
-import { makeStyles } from "@mui/styles";
 import {
   IconButton,
   LinearProgress,
-  Theme,
   Tooltip,
   Typography,
+  styled,
 } from "@mui/material";
 import { More } from "@mui/icons-material";
 import NavbarCategory from "./NavbarCategory";
 import { useWindowSize } from "../../hooks/windowSize";
 import { Link } from "react-router-dom";
 
-const useStyles = makeStyles((theme: Theme) => ({
-  container: {
-    display: "flex",
-    padding: "10px 30px",
-    position: "relative",
-    minHeight: 64,
-    alignItems: "center",
+const Container = styled("div")({
+  display: "flex",
+  padding: "10px 30px",
+  position: "relative",
+  minHeight: 64,
+  alignItems: "center",
+});
+
+const Content = styled("div")(({ theme }) => ({
+  flex: 0.6,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  [theme.breakpoints.down("md")]: {
+    flex: 1,
+    justifyContent: "end",
   },
-  content: {
-    flex: 0.6,
-    display: "flex",
-    justifyContent: "space-between",
+}));
+
+const Menu = styled("div")(({ theme }) => ({
+  display: "grid",
+  alignItems: "center",
+  gridTemplateColumns: "1fr 1fr 1fr",
+  flexDirection: "column",
+  position: "absolute",
+  padding: "12px 15px",
+  width: "100%",
+  minHeight: 350,
+  backgroundColor: "#fff",
+  zIndex: 40,
+  border: "1px solid #eee",
+  fontSize: 14,
+  transition: "all 0.1s linear",
+  opacity: 0,
+  justifyItems: "center",
+  transform: "translateY(-20px)",
+  "&.active": {
+    opacity: 1,
+    transform: "translateY(0)",
   },
-  categoryItem: {
-    textTransform: "uppercase",
-    color: "#666",
+  "& > a": {
+    display: "inline-flex",
+    marginRight: 0,
+    padding: 7,
     fontWeight: 600,
-  },
-  menu: {
-    display: "grid",
-    gridTemplateColumns: "1fr 1fr 1fr",
-    flexDirection: "column",
-    position: "absolute",
-    padding: "12px 15px",
-    width: "100%",
-    minHeight: 350,
-    backgroundColor: "#fff",
-    zIndex: 40,
-    border: "1px solid #eee",
-    fontSize: 14,
-    transition: "all 0.1s linear",
-    opacity: 0,
-    justifyItems: "center",
-    transform: "translateY(-20px)",
-    "&.active": {
-      opacity: 1,
-      transform: "translateY(0)",
-    },
-    "& > a": {
-      display: "inline-flex",
-      marginRight: 0,
-      padding: 7,
-      fontWeight: 600,
-      color: theme.palette.primary.main,
-    },
-    [theme.breakpoints.down("sm")]: {
-      display: "none",
-    },
+    color: theme.palette.primary.main,
   },
 }));
 
 let timer: NodeJS.Timeout;
 
 const NavbarCategories: React.FC = () => {
-  const classes = useStyles();
   const { categories, loading } = useApp();
   const { width } = useWindowSize();
   const [shownMenu, setShownMenu] = useState(false);
@@ -102,8 +98,8 @@ const NavbarCategories: React.FC = () => {
   }
 
   function handleOnMouseEnter() {
-    setShownMenu(true);
-    setViewMoreCategories(true);
+    setShownMenu((state) => !state);
+    setViewMoreCategories((state) => !state);
   }
 
   function handleOnMouseOverMenu() {
@@ -111,31 +107,31 @@ const NavbarCategories: React.FC = () => {
   }
 
   return (
-    <div
-      className={classes.container}
-      id="categories-link"
-      onMouseLeave={handleOnMouseLeave}
-    >
+    <Container id="categories-link" onMouseLeave={handleOnMouseLeave}>
       {loading ? (
         <LinearProgress style={{ width: "100%" }} />
       ) : (
         <>
           <Typography flex={0.4} fontWeight="bold">
-            SEÇÕES
+            SECTIONS
           </Typography>
-          <div className={classes.content}>
-            {categories.slice(0, 5).map((item) => (
-              <Link to={`/produtos?categoria=${item}`}>
-                <a className={classes.categoryItem}>{item}</a>
-              </Link>
-            ))}
-            <Tooltip title="+ categorias">
-              <IconButton onMouseEnter={handleOnMouseEnter}>
+          <Content>
+            {width >= 900 &&
+              categories.slice(0, 5).map((item, index) => (
+                <Link key={index} to={`/produtos?categoria=${item}`}>
+                  {item}
+                </Link>
+              ))}
+            <Tooltip title="+ categories">
+              <IconButton
+                onClick={handleOnMouseEnter}
+                onMouseEnter={handleOnMouseEnter}
+              >
                 <More />
               </IconButton>
             </Tooltip>
-          </div>
-          <div
+          </Content>
+          <Menu
             onClick={() => setShownMenu(false)}
             onMouseOver={handleOnMouseOverMenu}
             onTransitionEnd={
@@ -147,16 +143,19 @@ const NavbarCategories: React.FC = () => {
               top: rects.top,
               left: rects.left,
               visibility: viewMoreCategories ? "visible" : "hidden",
+              textAlign: "center",
             }}
-            className={shownMenu ? `${classes.menu} active` : classes.menu}
+            className={shownMenu ? "active" : ""}
           >
-            {categories.slice(5, categories.length).map((category) => (
-              <NavbarCategory category={category} />
-            ))}
-          </div>
+            {categories
+              .slice(width >= 900 ? 5 : 0, categories.length)
+              .map((category, index) => (
+                <NavbarCategory key={index} category={category} />
+              ))}
+          </Menu>
         </>
       )}
-    </div>
+    </Container>
   );
 };
 
